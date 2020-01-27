72baea7906abd9e2e2d3b230b1dba81e290cd53f
Paul Zimmerman <pcz@dalton.org>
2020-01-27 16:44:34 -0500

    Create the schedule_set_list SKY API endpoint.

--- skyapi/school/__init__.py
+++ skyapi/school/__init__.py
@@ -1,5 +1,6 @@
 from . import address_list_single_user
 from . import building_list
 from . import level_list
+from . import schedule_set_list
 from . import v1legacylistsbylist_idget
 from . import year_list
---
+++ skyapi/school/schedule_set_list.py
@@ -0,0 +1,28 @@
+def get(client: object, level_num: int, school_year: str = None, group_type: int = None) -> dict:
+    """Get the list of schedule sets for the level, school year, and group type indicated.
+
+    Documentation:
+        https://developer.sky.blackbaud.com/docs/services/school/operations/V1AcademicsSchedulesSetsGet
+
+    Args:
+        client (object): The SKY API client object.
+        school_year (str): The school year. Defaults to the current year if not specified.
+        group_type (int): The group type. Defaults to academic (1) if not specified.
+
+    Returns:
+        Dictionary of results.
+    """
+    client.rate_limiter()
+    params = {
+        'level_num': level_num,
+    }
+    if school_year:
+        params['school_year'] = school_year
+    if group_type:
+        params['group_type'] = group_type,
+    r = client.session.get(
+        f'https://api.sky.blackbaud.com/school/v1/academics/schedules/sets',
+        params=params,
+        headers=client.headers,
+    )
+    return r.json()
