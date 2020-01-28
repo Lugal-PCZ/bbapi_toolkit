242d145fb48be6ce560584a4cd0359b72b96e13b
Paul Zimmerman <pcz@dalton.org>
2020-01-28 10:24:12 -0500

    Create master_schedule_list SKY API endpoint.

--- skyapi/school/__init__.py
+++ skyapi/school/__init__.py
@@ -2,6 +2,7 @@ from . import address_list_single_user
 from . import building_list
 from . import cycles_by_section
 from . import level_list
+from . import master_schedule_list
 from . import schedule_set_list
 from . import schedule_set_single_item
 from . import term_list
---
+++ skyapi/school/master_schedule_list.py
@@ -0,0 +1,26 @@
+def get(client: object, level_num: int, start_date: str, end_date: str, offering_type: int = None) -> dict:
+    """Get the master schedule days within the date range indicated.
+
+    Documentation:
+        https://developer.sky.blackbaud.com/docs/services/school/operations/V1AcademicsSchedulesMasterGet
+
+    Args:
+        client (object): The SKY API client object.
+        level_num (int): The id of the school level.
+        start_date (str): Start of the date range (inclusive), in the format 'yyyy-mm-dd'.
+        end_date (str): End of the date range (inclusive), in the format 'yyyy-mm-dd'.
+        offering_type (int): Id of the group type. Defaults to all offering types if not specified.
+
+    Returns:
+        Dictionary of results.
+    """
+    client.rate_limiter()
+    params = {}
+    if offering_type:
+        params['offering_type'] = offering_type
+    r = client.session.get(
+        f'https://api.sky.blackbaud.com/school/v1/academics/schedules/master?level_num={level_num}&start_date={start_date}&end_date={end_date}',
+        params=params,
+        headers=client.headers,
+    )
+    return r.json()
