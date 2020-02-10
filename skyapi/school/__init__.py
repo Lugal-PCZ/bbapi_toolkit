e9f842c097400b27800252896c3c97db46a5ae8a
Paul Zimmerman <pcz@dalton.org>
2020-02-10 15:16:28 -0500

    Create section_list_by_school_level SKY API endpoint.

--- skyapi/school/__init__.py
+++ skyapi/school/__init__.py
@@ -5,6 +5,7 @@ from . import level_list
 from . import master_schedule_list
 from . import schedule_set_list
 from . import schedule_set_single_item
+from . import section_list_by_school_level
 from . import term_list
 from . import v1legacylistsbylist_idget
 from . import year_list
---
+++ skyapi/school/section_list_by_school_level.py
@@ -0,0 +1,23 @@
+def get(client: object, level_num: int, school_year: str = None) -> dict:
+    """Get the list of sections for the level and school year indicated.
+
+    Documentation:
+        https://developer.sky.blackbaud.com/docs/services/school/operations/V1AcademicsSectionsGet
+
+    Args:
+        client (object): The SKY API client object.
+        school_year (str): The school year. Defaults to the current year if not specified.
+
+    Returns:
+        Dictionary of results.
+    """
+    client.rate_limiter()
+    params = {'level_num': level_num}
+    if school_year:
+        params['school_year'] = school_year
+    r = client.session.get(
+        f'https://api.sky.blackbaud.com/school/v1/academics/sections',
+        params=params,
+        headers=client.headers,
+    )
+    return r.json()
